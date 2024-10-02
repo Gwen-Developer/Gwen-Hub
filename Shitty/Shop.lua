36b6bb4d65a9740f0d1eac0c429ccfb317d23248
Gwen-Developer <30754570+Gwen-Developer@users.noreply.github.com>
2024-10-02 17:47:36 +0700

    Create Shop.lua

---
+++ Shitty/Shop.lua
@@ -0,0 +1,55 @@
+local Shop = {
+    ["Shop Abilities"] = {
+        {"Geppo ($10K)", {"BuyHaki", "Geppo"}},
+        {"Buso ($25K)", {"BuyHaki", "Buso"}},
+        {"Soru ($100K)", {"BuyHaki", "Soru"}},
+        {"Observation Haki ($750K)", {"Kentalk", "Buy"}}
+    },
+    ["Shop Melees"] = {
+        {"Black Leg ($150K)", {"BuyBlackLeg"}},
+        {"Electro ($500K)", {"BuyElectro"}},
+        {"Fishman Karate ($750K)", {"BuyFishmanKarate"}},
+        {"Dragon Breath (ƒ1,500)", {"BlackbeardReward", "DragonClaw", "2"}},
+        {"Super Human ($3M)", {"BuySuperhuman"}},
+        {"Sharkman Karate ($2.5M) + (ƒ5,000)", {"BuySharkmanKarate"}},
+        {"Death Step ($2.5M) + (ƒ5,000)", {"BuyDeathStep"}},
+        {"Electric Claw ($3M) + (ƒ5,000)", {"BuyElectricClaw"}},
+        {"Dragon Talon ($3M) + (ƒ5,000)", {"BuyDragonTalon"}},
+        {"God Human ($3M)", {"BuyGodhuman"}}
+    },
+    ["Shop Swords"] = {
+        {"Katana ($1K)", {"BuyItem", "Katana"}},
+        {"Cutlass ($1K)", {"BuyItem", "Cutlass"}},
+        {"Dual Katana ($12K)", {"BuyItem", "Dual Katana"}},
+        {"Iron Mace ($25K)", {"BuyItem", "Iron Mace"}},
+        {"Triple Katana ($60K)", {"BuyItem", "Triple Katana"}},
+        {"Pipe ($100K)", {"BuyItem", "Pipe"}},
+        {"Dual-Headed Blade ($400K)", {"BuyItem", "Dual-Headed Blade"}},
+        {"Soul Cane ($750K)", {"BuyItem", "Soul Cane"}},
+        {"Bisento ($1.2M)", {"BuyItem", "Bisento"}}
+    },
+    ["Shop Guns"] = {
+        {"Slingshot ($5K)", {"BuyItem", "Slingshot"}},
+        {"Musket ($8K)", {"BuyItem", "Musket"}},
+        {"Flintlock ($10.5K)", {"BuyItem", "Flintlock"}},
+        {"Refined Slingshot ($30K)", {"BuyItem", "Refined Slingshot"}},
+        {"Refined Flintlock ($65K)", {"BuyItem", "Refined Flintlock"}},
+        {"Cannon ($100K)", {"BuyItem", "Cannon"}}
+    },
+    ["Shop Accessories"] = {
+        {"Black Cape ($50K)", {"BuyItem", "Black Cape"}},
+        {"Swordsman Hat ($150K)", {"BuyItem", "Swordsman Hat"}},
+        {"Tomoe Ring ($500K)", {"BuyItem", "Tomoe Ring"}}
+    },
+    ["Shop Fragment"] = {
+        {"Kabucha (ƒ1,500)", {"BlackbeardReward", "Slingshot", "2"}},
+        {"Race Reroll (ƒ3,000)", {"BlackbeardReward", "Reroll", "2"}},
+        {"Refund Stats (ƒ2,500)", {"BlackbeardReward", "Refund", "2"}}
+    },
+    ["Shop Ectoplasm"] = {
+        {"Bizarre Rifle (25)", {"Ectoplasm", "Buy", 1}},
+        {"Ghoul Mask (50)", {"Ectoplasm", "Buy", 2}},
+        {"Midnight Blade (100)", {"Ectoplasm", "Buy", 3}}
+    }
+}
+return Shop
