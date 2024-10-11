3c386a85549e9d594f3884d1d2a8776a644a5f5b
Gwen-Developer <30754570+Gwen-Developer@users.noreply.github.com>
2024-10-11 16:04:17 +0700

    Update Fastattack.lua

--- Shitty/Fastattack.lua
+++ Shitty/Fastattack.lua
@@ -1,67 +1,62 @@
-local Camera = require(ReplicatedStorage.Util.CameraShaker)
-Camera:Stop()
 local CombatFramework = require(Player.PlayerScripts:WaitForChild("CombatFramework"))
 local CombatFrameworkR = getupvalues(CombatFramework)[2]
-
-function CurrentWeapon()
-	local ac = CombatFrameworkR.activeController
-	local ret = ac.blades[1]
-	if not ret then return Player.Character:FindFirstChildOfClass("Tool").Name end
-	pcall(function()
-		while ret.Parent~=Player.Character do ret=ret.Parent end
-	end)
-	if not ret then return Player.Character:FindFirstChildOfClass("Tool").Name end
-	return ret
-end
-
-function AttackNoCD()
-	local AC = CombatFrameworkR.activeController
-	for i = 1,1 do 
-		local bladehit = require(ReplicatedStorage.CombatFramework.RigLib).getBladeHits(Player.Character, {Player.Character.HumanoidRootPart},60)
-		local cac = {}
-		local hash = {}
-		for k, v in pairs(bladehit) do
-			if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
-				table.insert(cac, v.Parent.HumanoidRootPart)
-				hash[v.Parent] = true
+local RigLib = require(ReplicatedStorage.CombatFramework.RigLib)
+require(Player.PlayerScripts.CombatFramework.Particle).play = function() end
+require(ReplicatedStorage.Util.CameraShaker):Stop()
+hookfunction(RigLib.wrapAttackAnimationAsync,function(p_u_28, p_u_29, p_u_30, p_u_31, p_u_32)
+    local ac = CombatFrameworkR.activeController
+    local v_u_36 = tick()
+    if ac and ac.equipped then
+        local v37 = RigLib.getBladeHits(p_u_29, p_u_30, p_u_31)
+        if #v37 > 0 then
+			p_u_32(v37)
+			if true and tick() - v_u_36 >= _G.Settings.Configs["Fast Attack Delay"] then
+				ReplicatedStorage.RigControllerEvent:FireServer("weaponChange", tostring(ac.currentWeaponModel))
 			end
-		end
-		bladehit = cac
-		if #bladehit > 0 then
-            local u8 = debug.getupvalue(AC.attack, 5)
-            local u9 = debug.getupvalue(AC.attack, 6)
-            local u7 = debug.getupvalue(AC.attack, 4)
-            local u10 = debug.getupvalue(AC.attack, 7)
-            local u12 = (u8 * 798405 + u7 * 727595) % u9
-            local u13 = u7 * 798405
-                (function()
-                    u12 = (u12 * u9 + u13) % 1099511627776
-                    u8 = math.floor(u12 / u9)
-                    u7 = u12 - u8 * u9
-                end)()
-            u10 = u10 + 1
-            debug.setupvalue(AC.attack, 5, u8)
-            debug.setupvalue(AC.attack, 6, u9)
-            debug.setupvalue(AC.attack, 4, u7)
-            debug.setupvalue(AC.attack, 7, u10)
+        end
+    end
+end)
+task.spawn(function()
+    for i = 1,5 do 
+        repeat task.wait(1) until Player.Character:FindFirstChildOfClass('Tool') and (Player.Character:FindFirstChildOfClass('Tool').ToolTip == 'Melee' or Player.Character:FindFirstChildOfClass('Tool').ToolTip == 'Sword')
+        local acc5 = debug.getupvalues(require(Player.PlayerScripts.CombatFramework))[2].activeController
+        if not acc5 or not acc5.equipped then 
+            repeat task.wait()
+            until acc5 and acc5.equipped
+        end
+        for i,v in pairs(acc5.data) do  
+            if typeof(v) == 'function' then 
+                hookfunction(v,function() end )
+            end
+        end
+        wait(3)
+    end
+end)
+getgenv().AttackFunction = function()
+    local ac = CombatFrameworkR.activeController
+    if ac and ac.equipped then
+        if not getgenv().CurrentCharHum or not getgenv().CurrentCharHum.Parent or getgenv().CurrentCharHum.ClassName ~='Humanoid' then 
+            getgenv().CurrentCharHum = Player.Character:FindFirstChildOfClass('Humanoid')
+        end
+        if (getgenv().CurrentCharHum and getgenv().CurrentCharHum.Parent.Stun.Value <= 0) then
+            ac.hitboxMagnitude = 60
+            ac.active = false
+            ac.blocking = false
+            ac.focusStart = 0
+            ac.hitSound = nil
+            ac.increment = 0
+            ac.timeToNextAttack = 0
+            ac.timeToNextBlock = 0
             pcall(function()
-                if Player.Character:FindFirstChildOfClass("Tool") and AC.blades and AC.blades[1] then
-                    AC.animator.anims.basic[1]:Play(0.01,0.01,0.01)
-                    ReplicatedStorage.RigControllerEvent:FireServer("weaponChange",tostring(CurrentWeapon()))
-                    ReplicatedStorage.Remotes.Validator:FireServer(math.floor(u12 / 1099511627776 * 16777215), u10)
-                    ReplicatedStorage.RigControllerEvent:FireServer("hit", bladehit, i, "")
-                end
+                ac:attack()
             end)
         end
     end
 end
-spawn(function()
-	while task.wait() do
-		pcall(function()
-			if _G.UseFAttack and Player.Character.Stun.Value == 0 then
-				AttackNoCD()
-				task.wait(_G.Settings.Configs["Fast Attack Delay"])
-			end
-		end)
-	end
+task.spawn(function()
+    while task.wait() do 
+        if _G.UseFAttack then 
+            AttackFunction()
+        end
+    end
 end)
