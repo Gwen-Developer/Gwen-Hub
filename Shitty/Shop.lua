140721406bad346049e55c03e953646c204233aa
Gwen-Developer <30754570+Gwen-Developer@users.noreply.github.com>
2025-04-25 15:24:46 +0700

    Update Shop.lua

--- Shitty/Shop.lua
+++ Shitty/Shop.lua
@@ -48,9 +48,9 @@ local Shop = {
         {"Refund Stats (ƒ2,500)", {"BlackbeardReward", "Refund", "2"}}
     },
     ["Shop Ectoplasm"] = {
-        {"Bizarre Rifle (𝑒25)", {"Ectoplasm", "Buy", 1}},
-        {"Ghoul Mask (𝑒50)", {"Ectoplasm", "Buy", 2}},
-        {"Midnight Blade (𝑒100)", {"Ectoplasm", "Buy", 3}}
+        {"Bizarre Rifle (€25)", {"Ectoplasm", "Buy", 1}},
+        {"Ghoul Mask (€50)", {"Ectoplasm", "Buy", 2}},
+        {"Midnight Blade (€100)", {"Ectoplasm", "Buy", 3}}
     }
 }
 return Shop
